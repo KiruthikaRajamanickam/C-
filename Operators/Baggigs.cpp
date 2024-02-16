9e59bee2d655cbae8a6a39e3e61ee8276281032c
everlesstutorials <160114200+everlesstutorials@users.noreply.github.com>
2024-02-16 21:53:11 +0530

    Create Baggigs.cpp

---
+++ Operators/Baggigs.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int a,b,c,d;
+    cin>>a>>b;
+    c=(pow(3-a,2))+(pow(4-b,2));
+    d=sqrt(c);
+    cout<<d;
+    return 0;
+}
