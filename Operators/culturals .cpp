7d12b69dcb73d573eb98edd70a350efdd7803de7
everlesstutorials <160114200+everlesstutorials@users.noreply.github.com>
2024-02-16 21:39:14 +0530

    Create culturals .cpp

---
+++ Operators/culturals .cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    int a,b,c,d;
+    cin>>a;
+    cin>>b;
+    c=a/b;
+    cout<<c<<'\n';
+    d=a-(b*c);
+    cout<<d;
+    return 0;
+    
+    
+    return 0;
+}
