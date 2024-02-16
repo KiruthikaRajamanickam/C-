5359acb84589ddd9dc8a40f18acffb8450a0cb25
everlesstutorials <160114200+everlesstutorials@users.noreply.github.com>
2024-02-16 21:23:25 +0530

    Create Dhoni endorments.cpp

---
+++ Input and Output/Dhoni endorments.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Pepsico"<<'\n'<<"AIRCEL"<<'\n'<<"Communications"<<'\n'<<"Titan"<<'\n'<<"TVS"<<'\n'<<"Motors"<<'\n'<<"Boost"<<'\n'<<"Reebok"<<'\n'<<"Videocon"<<'\n'<<"GE Money";
+}
