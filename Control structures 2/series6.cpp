83044b2b85629b3efb580d6e2b7dd3cf350a7cac
KIRUTHIKA R <160114200+KiruthikaRajamanickam@users.noreply.github.com>
2024-02-20 23:06:41 +0530

    Create series6.cpp

---
+++ Control structures 2/series6.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a;
+    for (int i=1; i<=a; i++)
+    {
+        b=pow(i,i);
+        cout<<b<<" ";
+    }
+    
+}
