cd13e5ee90014dbd732391a3e441f745c415b7eb
KIRUTHIKA R <160114200+KiruthikaRajamanickam@users.noreply.github.com>
2024-02-20 23:09:06 +0530

    Create series9.cpp

---
+++ Control structures 2/series9.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+      int n,first=11,a;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+       a=first*first;
+       cout<<a<<" ";
+    first+=4;
+    }
+    
+        return 0;
+}
