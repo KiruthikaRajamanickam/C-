4bdffd6cdbf7e5b3650ab07508cf2ccf7681514d
KIRUTHIKA R <160114200+KiruthikaRajamanickam@users.noreply.github.com>
2024-02-20 22:51:21 +0530

    Create chalk.cpp

---
+++ Control Structures I/chalk.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+#include<math.h>
+using namespace std;
+int main()
+{
+    float n,c;
+    cin>>n;
+    c=(sqrt(n)+n+1);
+    cout<<fixed<<setprecision(0)<<c;
+    return 0;
+}
