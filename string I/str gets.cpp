59a40ebe21307d69e795598f50abfa07db31ae23
KIRUTHIKA R <160114200+KiruthikaRajamanickam@users.noreply.github.com>
2024-02-22 21:57:38 +0530

    Create str gets.cpp

---
+++ string I/str gets.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+#include<cstring>
+using namespace std;
+int main() 
+{
+    char str[100];
+    cin.getline(str,100);
+    cout << "The string is ";
+    puts(str);
+    return 0;
+}
