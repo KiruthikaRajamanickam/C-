ae6cb65e010dddbbce5e8114a815ce9c2d690516
KIRUTHIKA R <160114200+KiruthikaRajamanickam@users.noreply.github.com>
2024-02-22 21:55:57 +0530

    Create str palindrome.cpp

---
+++ string I/str palindrome.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+int main()
+{
+    string a,b;
+    cin>>a;
+    reverse(b.begin(),b.end());
+    if(a==b)
+    {
+        cout<<"Panlindrome";
+    }
+    else
+    {
+        cout<<"Not a Palindrome";
+    }
+}
